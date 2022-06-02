1aafc08272b905849e13b04b834132d297bf958b
Анна Симанова <ani.simanova@yandex.ru>
2022-06-03 00:50:44 +0300

    wait-for add

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432')
     }
 }
